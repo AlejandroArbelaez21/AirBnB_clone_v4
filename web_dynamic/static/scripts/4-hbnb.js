126e5922800017db21a40108ec0ee74023d555f6
darkares23 <desarrollojuseca@gmail.com>
2020-05-18 17:55:02 -0500

    Fix filter buton to show serach results

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,4 +1,5 @@
 $(() => {
+  amenityIds = [];
   $.get('http://0.0.0.0:5001/api/v1/status/', (res) => {
     if (res.status === 'OK') {
       $('header #api_status').addClass('available');
@@ -9,14 +10,14 @@ $(() => {
       url: 'http://0.0.0.0:5001/api/v1/places_search/',
       type: 'POST',
       contentType: 'application/json',
-      data: JSON.stringify({ amenities: Object.keys('amenityIds') }),
+      data: JSON.stringify({amenities: Object.keys(amenityIds)}),
       success: function (data) {
         let listCheck = [];
         for (let index = 0; index < data.length; index++) {
           listCheck.push(createTemplate(data[index]));
         }
         listCheck = listCheck.join('');
-        $('section.places article').remove();
+        $('section.places > article').remove();
         $('section.places').append(listCheck);
       }
     });
@@ -40,26 +41,25 @@ $(() => {
       delete listId[this.dataset.id];
     }
     const listval = Object.values(listId);
-    $('.amenities h4').text(listval.join(', '));
+    $('.amenities h4').text(listval);
   });
 });
 function createTemplate (place) {
   return (`
       <article>
-        <div class="title_box">
-          <h2>{{ place.name }}</h2>
-          <div class="price_by_night">$${place.price_by_night}</div>
-        </div>
-        <div class="information">
-          <div class="max_guest">{{ place.max_guest }} Guest{% if place.max_guest != 1 %}s{% endif %}</div>
-          <div class="number_rooms">{{ place.number_rooms }} Bedroom{% if place.number_rooms != 1 %}s{% endif %}</div>
-          <div class="number_bathrooms">{{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}</div>
-        </div>
-        <div class="user">
-          <b>Owner:</b> {{ place.user.first_name }} {{ place.user.last_name }}
-        </div>
-        <div class="description">
-          {{ place.description | safe }}
-        </div>
-    </article>`);
+      <div class="title_box">
+        <h2>${place.name}</h2>
+        <div class="price_by_night">$${place.price_by_night}</div>
+      </div>
+      <div class="information">
+        <div class="max_guest">${place.max_guest} Guests</div>
+        <div class="number_rooms">${place.number_rooms} Rooms</div>
+        <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+      </div>
+      <div class="user">
+      </div>
+      <div class="description">
+        ${place.description}
+      </div>
+  </article>`);
 }
