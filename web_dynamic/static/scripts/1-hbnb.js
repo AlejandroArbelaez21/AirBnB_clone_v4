ab56c32cb3e9e366bed8bf56845071f3ccb98807
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-05-18 00:31:50 -0500

    fixing up the task 2 and upload task 3

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,12 @@
-
-const listId = {};
-$('input#amenChbox').change(() => {
-  if (this.checked) {
-    listId[this.dataset.id] = this.dataset.name;
-  } else {
-    delete listId[this.dataset.id];
-  }
-  const listVal = Object.values(listId);
-  $('.amenities h4').text(listVal.join(', '));
-});
+$(() => {
+    const listId = {};
+    $('.amenities .popover li input').change(function (event) {
+      if (this.checked) {
+        listId[this.dataset.id] = this.dataset.name;
+      } else {
+        delete listId[this.dataset.id];
+      }
+      const listval = Object.values(listId);
+      $('.amenities h4').text(listval.join(', '));
+    });
+  });
