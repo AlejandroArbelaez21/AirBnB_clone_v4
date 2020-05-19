a0db2ecab7fc48b666fce399af9d97b182485e02
darkares23 <desarrollojuseca@gmail.com>
2020-05-18 21:49:00 -0500

    fixed spaces when checkboxe is selwcted

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,13 @@
 $(() => {
-    const listId = {};
-    $('.amenities .popover li input').change(function (event) {
-      if (this.checked) {
-        listId[this.dataset.id] = this.dataset.name;
-      } else {
-        delete listId[this.dataset.id];
-      }
-      const listval = Object.values(listId);
-      $('.amenities h4').text(listval.join(', '));
-    });
+  const listId = {};
+  $('.amenities .popover li input').change(function () {
+    if ($(this).is(':checked')) {
+      listId[$(this).attr('data_id')] = $(this).attr('data-name');
+      console.log(listId);
+      
+    } else {
+      delete listId[this.dataset.id];
+    }
+    $('div.amenities h4').html(Object.values(listId).join(', ') || '&nbsp;');
   });
+});
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,17 +1,18 @@
 $(() => {
+  const listId = {};
+  $('.amenities .popover li input').change(function () {
+    if ($(this).is(':checked')) {
+      listId[$(this).attr('data_id')] = $(this).attr('data-name');
+      console.log(listId);
+      
+    } else {
+      delete listId[this.dataset.id];
+    }
+    $('div.amenities h4').html(Object.values(listId).join(', ') || '&nbsp;');
+  });
   $.get('http://0.0.0.0:5001/api/v1/status/', (res) => {
     if (res.status === 'OK') {
       $('header #api_status').addClass('available');
     }
-  });
-  const listId = {};
-  $('.amenities .popover li input').change(function (event) {
-    if (this.checked) {
-      listId[this.dataset.id] = this.dataset.name;
-    } else {
-      delete listId[this.dataset.id];
-    }
-    const listval = Object.values(listId);
-    $('.amenities h4').text(listval.join(', '));
   });
 });
